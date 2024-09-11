193e7479a5d9bbbed67627ba5984f8856ba12e2d
ChanZiXuan <167169327+ChanZiXuan@users.noreply.github.com>
2024-09-12 00:55:44 +0800

    Update modelTest.py

--- modelTest.py
+++ modelTest.py
@@ -57,10 +57,10 @@ def main():
         try:
             prediction_proba = lr_model.predict_proba(input_data_scaled)
             threshold = 0.3  # Lower the threshold to increase sensitivity
-                if prediction_proba[0][1] > threshold:
-                    st.write('This person has heart disease.')
-                else:
-                    st.write('This person does not have heart disease.')
+            if prediction_proba[0][1] > threshold:
+                st.write('This person has heart disease.')
+            else:
+                st.write('This person does not have heart disease.')
         except Exception as e:
             st.write(f'An error occurred during prediction: {e}')
 
